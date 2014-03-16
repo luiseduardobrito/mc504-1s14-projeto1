f2f142f2a71b21ebb7aac1c19bb6c08356441b9d
Luís Eduardo Brito <luiseduardo14@gmail.com>
2014-03-16 01:21:31 -0300

    checagem agora diz onde está o erro

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "src/checker.h"
 #include "src/solver.h"
@@ -23,20 +24,38 @@ void printBoard(int board[9][9]) {
 
 int main() {
 
+	printf("SUDOKU MULTIHTREAD\n\nComandos disponíveis:\n");
+	printf("1 )  Checar diagrama\n");
+	printf("2 )  Resolver diagrama\n");
+	printf("3 )  Modo dica\n\n\nEscolha um comando... ");
+
+	int cmd;
+	scanf("%d", &cmd);
+
 	int puzzle[BSIZE][BSIZE], i, j;
 
-	for(i = 0; i < BSIZE; i++)
-		for(j = 0; j < BSIZE; j++)
+	for (i = 0; i < BSIZE; i++)
+		for (j = 0; j < BSIZE; j++)
 			scanf("%d", &puzzle[i][j]);
 
-	if(!solve_sudoku(puzzle, 0 , 0)) {
-		printf("\n\n fudeu");
-	}
+	switch(cmd) {
+		case 1:
+			check_board(puzzle);
+			break;
+		case 2:
 
-	else {
-		printBoard(puzzle);
+			solve_sudoku(puzzle, 0, 0);
+			printf("\n\n\nDiagrama resolvido:\n\n");
+			printBoard(puzzle);
+			break;
+
+		case 3:
+			hint_sudoku(puzzle);
+
+			printf("\n\n\nDiagrama preenchido com dicas:");
+			printBoard(puzzle);
+			break;
 	}
 
 	return 0;
 }
-
--- src/checker.c
+++ src/checker.c
@@ -7,56 +7,107 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
 
 #include "const.h"
 #include "checker.h"
 
-pthread_t thr_lines[BSIZE];
+#define NUM_THREADS	3
 
-void check_board(int puzzle[BSIZE][BSIZE]) {
-	check_lines(puzzle);
-	check_columns(puzzle);
-	check_blocks(puzzle);
-}
+char *messages[NUM_THREADS];
 
-int partition(int a[], int l, int r) {
-	int pivot, i, j, t;
-	pivot = a[l];
-	i = l;
-	j = r + 1;
+int PUZZLE[BSIZE][BSIZE];
 
-	while (1) {
-		do
-			++i;
-		while (a[i] <= pivot && i <= r);
-		do
-			--j;
-		while (a[j] > pivot);
-		if (i >= j)
-			break;
-		t = a[i];
-		a[i] = a[j];
-		a[j] = t;
+int foundError = 0;
+
+struct thread_data {
+	int thread_id;
+	int type;
+};
+
+struct thread_data thread_data_array[NUM_THREADS];
+
+void *check_board_thr(void *thread_args) {
+
+	struct thread_data *my_data = (struct thread_data *) thread_args;
+	int type = my_data->type;
+
+	switch (type) {
+
+	case 0:
+		check_lines(PUZZLE);
+		break;
+	case 1:
+		check_columns(PUZZLE);
+		break;
+	case 2:
+		check_blocks(PUZZLE);
+		break;
 	}
-	t = a[l];
-	a[l] = a[j];
-	a[j] = t;
-	return j;
+
+	pthread_exit(NULL);
 }
 
+void check_board(int puzzle[][BSIZE]) {
+
+	int i, j, rc;
+
+	for (i = 0; i < BSIZE; i++)
+		for (j = 0; j < BSIZE; j++)
+			PUZZLE[i][j] = puzzle[i][j];
+
+	pthread_t threads[NUM_THREADS];
+
+	for (i = 0; i < NUM_THREADS; i++) {
+
+		thread_data_array[i].thread_id = i;
+		thread_data_array[i].type = i;
+
+		printf("Criando thread %d...\n", i + 1);
+
+		rc = pthread_create(&threads[i], NULL, check_board_thr,
+				(void *) &thread_data_array[i]);
+		if (rc) {
+			printf("ERRO: erro de retorno foi %d\n", rc);
+			exit(-1);
+		}
+	}
+
+	for (i = 0; i < NUM_THREADS; i++) {
+		pthread_join(threads[i], NULL);
+	}
+
+	printf("\n\nChecagem completa. ");
+	if (!foundError) {
+		printf("Nenhum erro encontrado.");
+	}
+	printf("\n\n");
+}
+
+int cmpfunc(const void * a, const void * b) {
+	return (*(int*) a - *(int*) b);
+}
 void quickSort(int a[], int l, int r) {
-	int j;
-
-	if (l < r) {
-		// divide and conquer
-		j = partition(a, l, r);
-		quickSort(a, l, j - 1);
-		quickSort(a, j + 1, r);
-	}
+	qsort(a, BSIZE, sizeof(int), cmpfunc);
 }
 
-void findError(int aux[9]) {
+void findError(int aux[9], int type, int pos) {
+
+	char str[20];
+
+	switch (type) {
+		case 1:
+			strcpy(str, "na linha");
+			break;
+		case 2:
+			strcpy(str, "na coluna");
+			break;
+
+		case 3:
+			strcpy(str, "no bloco");
+			break;
+	}
 
 	int i;
 
@@ -66,53 +117,70 @@ void findError(int aux[9]) {
 	//acha numeros repetidos e fora do intervalo
 	for (i = 0; i < 9; i++) {
 		if (aux[i] > 9 || aux[i] < 1) {
-			printf("Número %d fora do intervalo definido", aux[i]);
+
+			printf("Número %d fora do intervalo definido %s de numero %d\n", aux[i],
+					str, pos);
+			foundError = 1;
 		}
 
-		if (aux[i] == aux[i + 1]) {
-			printf("Número %d repetido", aux[i]);
+		if (i < 8 && aux[i] == aux[i + 1]) {
+			printf("Número %d repetido %s de numero %d\n", aux[i], str, pos);
+			foundError = 1;
 		}
 	}
-
 }
 
-void check_lines(int puzzle[BSIZE][BSIZE]) {
+void check_lines(int puzzle[][BSIZE]) {
+
 	int row, col;
 	int aux[BSIZE];
 
 	for (row = 0; row < BSIZE; row++) {
+
+		// Procura erro na linha atual
 		for (col = 0; col < BSIZE; col++) {
 			aux[col] = puzzle[row][col];
 		}
-		findError(aux);
+
+		// Procura erro na linha atual
+		findError(aux, 1, row + 1);
 	}
 }
 
-void check_columns(int puzzle[BSIZE][BSIZE]) {
+void check_columns(int puzzle[][BSIZE]) {
 	int row, col;
 	int aux[BSIZE];
 
 	for (col = 0; col < BSIZE; col++) {
+
+		// Procura erro na linha atual
 		for (row = 0; row < BSIZE; row++) {
 			aux[row] = puzzle[row][col];
 		}
-		findError(aux);
+
+		// Procura erro na coluna atual
+		findError(aux, 2, col + 1);
 	}
 }
 
-void check_blocks(int puzzle[BSIZE][BSIZE]) {
+void check_blocks(int puzzle[][BSIZE]) {
 
 	int i;
 
-	int rowStart = 0, colStart = 0;
+	int rowStart = 0, colStart = 0, blockNum;
 	int aux[BSIZE];
 
 	for (rowStart = 0; rowStart < 7; rowStart += 3) {
+
 		for (colStart = 0; colStart < 7; colStart += 3) {
+
 			for (i = 0; i < BSIZE; i++) {
 				aux[i] = puzzle[rowStart + (i % 3)][colStart + (i / 3)];
 			}
-			findError(aux);
+
+			blockNum++;
+
+			findError(aux, 3, blockNum);
 		}
 	}
 }
